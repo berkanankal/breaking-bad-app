d43998998fe98ec5c088af49a451754940f55ffc
brknkl <berkanankal68@gmail.com>
2022-01-02 13:24:38 +0300

    fetch characters completed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,42 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCharacters } from "../../redux/charactersSlice";
+import Masonry from "react-masonry-css";
+import "./styles.css";
 
 const Home = () => {
+  const dispatch = useDispatch();
   const characters = useSelector((state) => state.characters.items);
+  const error = useSelector((state) => state.characters.error);
+  const isLoading = useSelector((state) => state.characters.isLoading);
   console.log(characters);
 
-  return <div>Hello world!</div>;
+  useEffect(() => {
+    dispatch(fetchCharacters());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>...Loading</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <Masonry
+      breakpointCols={4}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {characters.map((character) => (
+        <div key={character.char_id}>
+          <img alt={character.name} src={character.img} width="100%" />
+          <div className="char_name">{character.name}</div>
+        </div>
+      ))}
+    </Masonry>
+  );
 };
 
 export default Home;
--- src/redux/charactersSlice.js
+++ src/redux/charactersSlice.js
@@ -1,11 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchCharacters = createAsyncThunk(
+  "characters/getAllCharacters",
+  async () => {
+    return axios
+      .get(
+        `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10&offset=0`
+      )
+      .then((res) => res.data);
+  }
+);
 
 export const charactersSlice = createSlice({
   name: "characters",
   initialState: {
     items: [],
+    isLoading: false,
+    error: null,
   },
   reducers: {},
+  extraReducers: {
+    [fetchCharacters.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchCharacters.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    [fetchCharacters.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    },
+  },
 });
 
 export default charactersSlice.reducer;
